115afc0f057f402b928b8c6b17fc4c7d4f53779b
Adeoye Moses <adeoyemoses2001@gmail.com>
2024-10-16 19:25:05 +0100

    iPhone 15 landing page

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://c14c9450ed249c944f51004ae239aa5b@o4508131876929536.ingest.us.sentry.io/4508131881058304",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
